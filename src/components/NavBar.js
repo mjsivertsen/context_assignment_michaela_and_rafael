8a24362d913373896e47222891aec7ea11cb6982
Michaela Sivertsen <mjsivertsen@msn.com>
2021-09-21 14:24:06 -0600

    setup

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+const NavBar = () => {
+return(
+  <Menu>
+    <Menu.Item>Home</Menu.Item>
+  </Menu>
+
+)
+
+
+}
+
+export default NavBar;
